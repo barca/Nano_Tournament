c1a3d2be249922cf8203e029f9b82316d292da9f
barca <tharden@wesleyan.edu>
2015-08-04 23:30:05 +0300

    starting to write python functions

---
+++ tournament.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.4
+#
+# Small script to show PostgreSQL and Pyscopg together
+#
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect("")
+except:
+    print "I am unable to connect to the database"
+
+c = conn.cursor()
+
+def deletePlayers():
+  c.execute("DELETE * FROM tournament")
+
